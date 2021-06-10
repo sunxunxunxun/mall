b91023f491e0002e5d773e1bae422583effd59b3
sunxunxunxun <1553035744@qq.com>
2021-06-10 14:14:34 +0800

    config and start nacos

--- mall-product/src/main/java/com/sunxun/mall/product/MallProductApplication.java
+++ mall-product/src/main/java/com/sunxun/mall/product/MallProductApplication.java
@@ -3,9 +3,11 @@ package com.sunxun.mall.product;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @MapperScan("com.sunxun.mall.product.dao")
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MallProductApplication {
 
     public static void main(String[] args) {
