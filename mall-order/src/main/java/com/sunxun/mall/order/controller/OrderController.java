ead18ed58b4580bebd24cc8b6d427eebe7e50f0f
sunxunxunxun <1553035744@qq.com>
2021-06-16 20:12:56 +0800

    test nacos configuration management

--- mall-order/src/main/java/com/sunxun/mall/order/controller/OrderController.java
+++ mall-order/src/main/java/com/sunxun/mall/order/controller/OrderController.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,11 +26,21 @@ import com.sunxun.common.utils.R;
  * @email sunxun1116@gmail.com
  * @date 2021-06-09 21:10:17
  */
+@RefreshScope
 @RestController
 @RequestMapping("order/order")
 public class OrderController {
     @Autowired
     private OrderService orderService;
+
+    @Value("${order.user.name}")
+    private String name;
+    @Value("${order.user.age}")
+    private Integer age;
+    @RequestMapping("/test")
+    public R test() {
+        return R.ok().put("name", name).put("age", age);
+    }
 
     @RequestMapping("/user/list")
     public R userorders() {
